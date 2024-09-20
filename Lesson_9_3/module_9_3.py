792b6925f723f2a3a1d91891d5482bb6332a3d9a
Timur958-mart <martusenko.timur@mail.ru>
2024-09-21 00:21:35 +0300

    Редактирование модуля module_9_3

--- Lesson_9_3/module_9_3.py
+++ Lesson_9_3/module_9_3.py
@@ -1,7 +1,7 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (abs(len(i[0]) - len(i[1])) for i in zip(first, second) if len(i[0]) != len(i[1]))
+first_result = (abs(len(i) - len(j)) for i, j in zip(first, second) if len(i) != len(j))
 second_result = (len(first[x]) == len(second[y]) for x in range(len(first)) for y in range(len(second)) if x == y)
 
 print(list(first_result))
